59a1ef8e7a75520cc431edd90b4754919f772017
andrew-best-diaxion <abest@diaxion.com>
2018-04-10 07:56:54 +1000

    confluence data migration streamlining
    New approach should be quicker, easier and more reliable to copy backup data to destination server.

--- MigrateConfluencePrep.sh
+++ MigrateConfluencePrep.sh
@@ -10,49 +10,70 @@
 # - you will need to put this script on the source CONFLUENCE server.
 
 # pseudo code
-# - stop CONFLUENCE
-# - tar up app dir (also includes the xml backups)
-# - prompt to start local CONFLUENCE server or not
-# - copy tar to destination server
-# - clean up local tarball
+
 
 # global VARs
 export CONFLUENCEAPPDIR="/var/atlassian/application-data/confluence"
+export CONFLUENCEBACKUPSDIR="$CONFLUENCEAPPDIR/backups"
+export CONFLUENCERESTOREDIR="$CONFLUENCEAPPDIR/restore"
 export CONFLUENCEDESTUSER="ubuntu"
 export CONFLUENCEDESTSERVER="diatapp01.westus2.cloudapp.azure.com"
 export CONFLUENCESTART=""
-export ZIPARCHIVENAME=`date "+%Y%m%d-%H%M%S"`
+#export ZIPARCHIVENAME=`date "+%Y%m%d-%H%M%S"`
+export SSHOPTIONS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
 # code
 #
 # be in your home dir
 cd ~ || exit
 
-# stop CONFLUENCE
-if [[ -x /opt/atlassian/confluence/bin/stop-confluence.sh ]] ;then
-  sudo /opt/atlassian/confluence/bin/stop-confluence.sh
-  # are we going to start CONFLUENCE after weve grabbed our tar ball?
-  echo "Do you wish to start CONFLUENCE again after weve prep'd our tarball?"
-  select yn in "Yes" "No"; do
-    case $yn in
-      Yes ) CONFLUENCESTART=1 ; echo "CONFLUENCE will be started"; break ;;
-      No ) CONFLUENCESTART=0 ; echo "CONFLUENCE WONT be started. You will need to start it manually."; exit ;;
-    esac
-  done
-  # zip up app dir
-  sudo tar -cvzf ./confluence-application-$ZIPARCHIVENAME.tar.gz $CONFLUENCEAPPDIR
-  # we have our tar ball now, lets start CONFLUENCE if we said we want to.
-  if [[ $CONFLUENCESTART ]] ; then
-    echo "restarting CONFLUENCE."
-    sudo /opt/atlassian/confluence/bin/start-confluence.sh
-  else
-    echo "CONFLUENCE not running."
-  fi
-  # copy the home dir to the destination server.
-  echo "Copying tarball to $CONFLUENCEDESTSERVER"
-  scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ./confluence-application-$ZIPARCHIVENAME.tar.gz $CONFLUENCEDESTUSER@$CONFLUENCEDESTSERVER:~/
-  rm -f ./confluence-application-$ZIPARCHIVENAME.tar.gz
-else
-  echo "/opt/atlassian/confluence/bin/stop-confluence.sh not found."
-  exit 1
-fi
+################################################################################
+# new code.
+# scp latest backup file to destination.
+# ssh to destination host and move backup file to correct location.
+################################################################################
+# put our latest backup file name into a VAR
+LATESTCONFLUENCEBACKUPFILE=$(sudo find $CONFLUENCEBACKUPSDIR/ -type f -printf '%p\n' | sort | head -n 1)
+# scp latest backup file to destination.
+sudo scp -i ~$CONFLUENCEDESTUSER/.ssh/id_rsa $SSHOPTIONS $LATESTCONFLUENCEBACKUPFILE $CONFLUENCEDESTUSER@$CONFLUENCEDESTSERVER:~/
+# ssh to destination host and move backup file to correct location.
+ssh $SSHOPTIONS $CONFLUENCEDESTUSER@$CONFLUENCEDESTSERVER sudo cp ./$LATESTCONFLUENCEBACKUPFILE $CONFLUENCERESTOREDIR
+#sudo cp `sudo find ./var/atlassian/application-data/confluence/backups/ -type f -printf '%p\n' | sort | head -n 1` $CONFLUENCERESTOREDIR
+#scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ./confluence-application-$ZIPARCHIVENAME.tar.gz $CONFLUENCEDESTUSER@$CONFLUENCEDESTSERVER:~/
+
+################################################################################
+# original code.
+# - stop CONFLUENCE
+# - tar up app dir (also includes the xml backups)
+# - prompt to start local CONFLUENCE server or not
+# - copy tar to destination server
+# - clean up local tarball
+################################################################################
+# # stop CONFLUENCE
+# if [[ -x /opt/atlassian/confluence/bin/stop-confluence.sh ]] ;then
+#   sudo /opt/atlassian/confluence/bin/stop-confluence.sh
+#   # are we going to start CONFLUENCE after weve grabbed our tar ball?
+#   echo "Do you wish to start CONFLUENCE again after weve prep'd our tarball?"
+#   select yn in "Yes" "No"; do
+#     case $yn in
+#       Yes ) CONFLUENCESTART=1 ; echo "CONFLUENCE will be started"; break ;;
+#       No ) CONFLUENCESTART=0 ; echo "CONFLUENCE WONT be started. You will need to start it manually."; exit ;;
+#     esac
+#   done
+#   # zip up app dir
+#   sudo tar -cvzf ./confluence-application-$ZIPARCHIVENAME.tar.gz $CONFLUENCEAPPDIR
+#   # we have our tar ball now, lets start CONFLUENCE if we said we want to.
+#   if [[ $CONFLUENCESTART ]] ; then
+#     echo "restarting CONFLUENCE."
+#     sudo /opt/atlassian/confluence/bin/start-confluence.sh
+#   else
+#     echo "CONFLUENCE not running."
+#   fi
+#   # copy the home dir to the destination server.
+#   echo "Copying tarball to $CONFLUENCEDESTSERVER"
+#   scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ./confluence-application-$ZIPARCHIVENAME.tar.gz $CONFLUENCEDESTUSER@$CONFLUENCEDESTSERVER:~/
+#   rm -f ./confluence-application-$ZIPARCHIVENAME.tar.gz
+# else
+#   echo "/opt/atlassian/confluence/bin/stop-confluence.sh not found."
+#   exit 1
+# fi
